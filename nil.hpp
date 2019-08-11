a85c351ba3f3829ee46ade0c76069f9185b9a4d0
Nozomi <40842954+z0w1non@users.noreply.github.com>
2019-08-11 18:16:58 +0900

    Add nil.hpp

---
+++ nil.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef DEFINED_NIL_HPP
+#define DEFINED_NIL_HPP
+#include <optional>
+
+/**
+ * nil represents an invalid valuu that assignable to any pointer type and std::optional<T> class.
+ * std::optional<int> opt = nil; assert(opt == std::nullopt);
+ * int * ptr = nil;              assert(ptr == nullptr);
+ *
+ */
+const class {
+public:
+	template<typename T>
+	operator T* () const {
+			return 0;
+		}
+
+	template<class C, class T>
+	operator T C::* () const {
+		return 0;
+	}
+
+	operator std::nullopt_t() const {
+		return std::nullopt;
+	}
+
+private:
+	void operator &() const;
+} nil;
+
+#endif
