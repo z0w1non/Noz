fe20b5e63c94fd28171265afe2e22a13dd359db1
Nozomi <40842954+z0w1non@users.noreply.github.com>
2019-08-11 18:19:24 +0900

    Add def.hpp / undef.hpp

---
+++ def.hpp
@@ -0,0 +1,8 @@
+#define cls class
+#define def using
+#define gen template
+#define jmp goto
+#define var auto
+#define ret return
+#define use using namespace
+#define op operator
---
+++ undef.hpp
@@ -0,0 +1,8 @@
+#undef cls
+#undef def
+#undef gen
+#undef jmp
+#undef var
+#undef ret
+#undef use
+#undef op
