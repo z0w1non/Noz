7ae03befe7cfd7cfd79368c80f72ece1755033ee
Nozomi <40842954+z0w1non@users.noreply.github.com>
2019-08-14 01:35:56 +0900

    Test implementation

--- output.hpp
+++ output.hpp
@@ -1,197 +1,243 @@
 #ifndef NOZ_OUTPUT_HPP
 #define NOZ_OUTPUT_HPP
 
+#include <cstdio>
+
 namespace Noz {
 
+template<typename>
+class let;
+
+template<typename>
+struct snprintf;
+
+template<>
+struct snprintf<char> {
+    template<typename ... args_t>
+    auto operator()(args_t && ... args) const { return std::snprintf(args ...); }
+};
+
+template<>
+struct snprintf<wchar_t> {
+    template<typename ... args_t>
+    auto operator()(args_t && ... args) const { return std::swprintf(args ...); }
+};
+
 template<
-    typename T,
+    typename base_type,
     typename CharT,
     typename Traits = std::char_traits<CharT>
 >
-class BaseOutput {
+class base_output {
 public:
     auto write_char(CharT c)
         -> void
     {
-        static_cast<T*>(this)->get_impl() << c;
+        get_ostream() << c;
     }
 
-    template<typename String>
-    auto write_line(const String & line)
+    auto write_line()
         -> void
     {
-        if (line.length() > 0 && line[line.length() - 1] == '\n') {
-            line.erase(line.length() - 1);
-        }
-        if (line.length() > 0 && line[line.length() - 1] == '\r') {
-            line.erase(line.length() - 1);
-        }
-        static_cast<T *>(this)->get_impl() << line << std::endl;
+        get_ostream() << std::endl;
     }
 
-    template<typename String>
-    auto write_all(const String & all)
+    template<typename ... string_types>
+    auto write_line(string_types ... strings)
         -> void
     {
-        static_cast<T *>(this)->get_impl() << all;
+        write(std::forward<string_types>(strings) ...);
+        write_line();
     }
 
-    template<typename U>
-    auto write(const U & obj)
+    template<typename char_type>
+    auto write_line(const char_type * ptr, std::size_t length)
         -> void
     {
-        static_cast<T*>(this)->get_impl() << obj;
+        write_line(ptr, length);
+    }
+
+    template<typename input_type>
+    auto write(input_type && input_value)
+        -> void
+    {
+        get_ostream() << std::forward<input_type>(input_value);
+    }
+
+    template<typename input_type, typename ... cdr_t>
+    auto write(input_type && input_value, cdr_t ... cdr)
+        -> void
+    {
+        write(std::forward<input_type>(input_value));
+        write(std::forward<cdr_t>(cdr) ...);
+    }
+
+    template<typename type>
+    auto write_binary(const type * pointer, std::size_t length)
+        -> void
+    {
+        const char * char_pointer = reinterpret_cast<const char *>(pointer);
+        get_ostream().write(char_pointer, length);
     }
 
     explicit operator bool() const {
-        return static_cast<bool>(static_cast<T*>(this)->get_impl());
+        return static_cast<bool>(get_ostream());
     }
 
-    class TemporaryBaseOutput {
-    public:
-        TemporaryBaseOutput(T & instance) : instance(instance) {}
-        TemporaryBaseOutput(const TemporaryBaseOutput &) = delete;
-        TemporaryBaseOutput(TemporaryBaseOutput && obj) :
-            instance{obj.instance},
-            has_ownership{obj.has_ownership}
-        { obj.has_ownership = false; }
+    template<typename ... argment_types>
+    int printf(const CharT * format, argment_types && ... argments) {
+        int length;
+        std::size_t buffer_size = Traits::length(format) + sizeof ... (argments) * 256;
+        while (true) {
+            std::unique_ptr<CharT[]> buffer = std::unique_ptr<CharT[]>(new CharT[buffer_size]);
+            length = snprintf<CharT>{}(buffer.get(), buffer_size, format, std::forward<argment_types>(argments) ...);
+            if (length > 0) {
+                write(buffer.get());
+                break;
+            }
+            buffer_size *= 2;
+        }
+        return length;
+    }
 
-        ~TemporaryBaseOutput() {
+    class temporary_base_output {
+    public:
+        temporary_base_output(base_output & base_output_instance) noexcept
+            : base_output_instance{base_output_instance}
+            , has_ownership{true}
+        {
+        }
+
+        temporary_base_output(const temporary_base_output &) = delete;
+
+        temporary_base_output(temporary_base_output && obj) noexcept
+            : base_output_instance{obj.base_output_instance}
+            , has_ownership{obj.has_ownership}
+        {
+            obj.has_ownership = false;
+        }
+
+        ~temporary_base_output() {
             if (has_ownership) {
-                auto & impl = static_cast<T&>(instance).get_impl();
-                impl << std::endl;
+                base_output_instance.write_line();
             }
         }
 
-        template<typename U>
-        auto operator ,(const U & obj)
-            -> TemporaryBaseOutput
+        template<typename input_type>
+        auto operator ,(const input_type & input_value)
+            -> temporary_base_output
         {
-            auto & impl = static_cast<T&>(instance).get_impl();
-            impl << obj;
-            return std::move(*this);
+            base_output_instance.get_ostream() << input_value;
+            return temporary_base_output(std::move(*this));
         }
 
     private:
-        T & instance;
-        bool has_ownership{true};
+        base_output & base_output_instance;
+        bool has_ownership;
     };
 
-    template<typename U>
-    auto operator <(U & instance)
-        -> TemporaryBaseOutput
+    template<typename input_type>
+    auto operator <(input_type && input_value)
+        -> temporary_base_output
     {
-        auto & impl = static_cast<T*>(this)->get_impl();
-        impl << instance;
-        return *static_cast<T*>(this);
+        get_ostream() << std::forward<input_type>(input_value);
+        return temporary_base_output{get_base()};
+    }
+
+private:
+    auto & get_base() {
+        return *static_cast<base_type *>(this);
+    }
+
+    const auto & get_base() const {
+        return *static_cast<const base_type *>(this);
+    }
+
+    auto & get_ostream() {
+        return get_base().get_ostream();
+    }
+
+    const auto & get_ostream() const {
+        return get_base().get_ostream();
     }
 };
 
-template<typename ... T>
-class BasicOutput;
+template<typename ...>
+class ostream_tag;
 
 template<
     typename CharT,
     typename Traits
 >
-class BasicOutput<std::basic_ofstream<CharT, Traits>>
-    : public BaseOutput<
-        BasicOutput<std::basic_ofstream<CharT, Traits>>,
-        CharT,
-        Traits
-    >
+class let<ostream_tag<CharT, Traits>>
+    : public base_output<let<ostream_tag<CharT, Traits>>, CharT, Traits>
 {
-    friend class BaseOutput<
-        BasicOutput<std::basic_ofstream<CharT, Traits>>,
-        CharT,
-        Traits
-    >;
+    friend class base_output<let<ostream_tag<CharT, Traits>>, CharT, Traits>;
 
 public:
-    template<typename ... Args>
-    BasicOutput(Args && ... args)
-        : out{std::forward<Args>(args) ...}
+    template<typename ostream_type>
+    let(ostream_type && ostream)
+        : ostream_holder{new ostream_holder_impl<ostream_type>>(std::forward<ostream_type>(ostream))}
     {}
 
 private:
-    auto get_impl()
-        -> std::basic_ofstream<CharT, Traits> &
-    {
-        return out;
-    }
+    struct ostream_holder_interface {
+    public:
+        virtual ~ostream_holder_interface() {}
+        virtual auto get() -> std::basic_ostream<CharT, Traits> & = 0;
+    };
 
-    auto get_impl() const
-        -> const std::basic_ofstream<CharT, Traits> &
-    {
-        return out;
-    }
+    template<typename ...>
+    struct ostream_holder_impl;
 
-    std::basic_ofstream<CharT, Traits> out;
-};
+    template<typename ostream_type>
+    struct ostream_holder_impl<ostream_type &> {
+        ostream_holder_impl(ostream_type & ostream) : ostream{ostream} {}
+        virtual ~ostream_holder_impl() {}
+        virtual auto get() -> std::basic_ostream<CharT, Traits> & { return ostream; }
+        ostream_type & ostream;
+    };
 
-template<
-    typename CharT,
-    typename Traits
->
-class BasicOutput<std::basic_ostream<CharT, Traits>>
-    : public BaseOutput<
-        BasicOutput<std::basic_ostream<CharT, Traits>>,
-        CharT,
-        Traits
-    >
-{
-    friend class BaseOutput<
-        BasicOutput<std::basic_ostream<CharT, Traits>>,
-        CharT,
-        Traits
-    >;
-public:
-    BasicOutput(std::basic_ostream<CharT, Traits> & out)
-        : out{out}
-    {}
+    template<typename ostream_type>
+    struct ostream_holder_impl<ostream_type &&> {
+        ostream_holder_impl(ostream_type && ostream) : ostream{std::move<ostream>} {}
+        virtual ~ostream_holder_impl() {}
+        virtual auto get() -> std::basic_ostream<CharT, Traits> & { return ostream; }
+        ostream_type ostream;
+    };
 
-private:
-    auto get_impl()
+    std::unique_ptr<ostream_holder_interface> ostream_holder;
+
+    auto get_ostream()
         -> std::basic_ostream<CharT, Traits> &
     {
-        return out;
+        return ostream_holder->get();
     }
 
-    auto get_impl() const
+    auto get_ostream() const
         -> const std::basic_ostream<CharT, Traits> &
     {
-        return out;
+        return ostream_holder->get();
     }
-
-    std::basic_ostream<CharT, Traits> & out;
 };
 
-using Output = BasicOutput<std::basic_ofstream<char>>;
-using WOutput = BasicOutput<std::basic_ofstream<wchar_t>>;
+template<typename char_type, typename traits = std::char_traits<char_type>>
+using basic_ostream = let<ostream_tag<char_type, traits>>;
+using Output = basic_ostream<char>;
+using WOutput = basic_ostream<wchar_t>;
 
-template<
-    typename CharT,
-    typename Traits = std::char_traits<CharT>,
-    typename Allocator = std::allocator<CharT>
->
-BasicOutput(BasicString<CharT, Traits, Allocator>)
-    -> BasicOutput<std::basic_ofstream<CharT, Traits> >;
+template<typename CharT, typename Traits = std::char_traits<CharT>>
+let(std::basic_ostream<CharT, Traits> &)
+    ->let<ostream_tag<CharT, Traits>>;
 
-template<typename CharT>
-BasicOutput(const CharT *)
-    -> BasicOutput<std::basic_ofstream<CharT>>;
+template<typename CharT, typename Traits = std::char_traits<CharT>>
+let(std::basic_ofstream<CharT, Traits> &&)
+    ->let<ostream_tag<CharT, Traits>>;
 
-template<
-    typename CharT,
-    typename Traits = std::char_traits<CharT>
->
-BasicOutput(std::basic_ostream<CharT, Traits> &)
-    -> BasicOutput<std::basic_ostream<CharT, Traits>>;
-
-BasicOutput out{std::cout};
-BasicOutput wout(std::wcout);
-BasicOutput err{std::cerr};
-BasicOutput werr(std::wcerr);
+let cout{std::cout};
+//let wcout{std::wcout};
+//let cerr{std::cerr};
+//let wcerr{std::wcerr};
 
 } // namespace Noz
 
