a16bd309745a3a8a59de4d912beba934bac5200a
z0w1non <z0w1non@gmail.com>
2019-09-23 09:56:43 +0900

    Replace tab to space

--- test.cpp
+++ test.cpp
@@ -53,7 +53,7 @@ BOOST_AUTO_TEST_CASE(nil_assignable)
 {
     int * ptr = nil;
     BOOST_CHECK_EQUAL(ptr, nullptr);
-	std::optional<int> opt = nil;
+    std::optional<int> opt = nil;
     BOOST_CHECK(!opt.has_value());
     BOOST_CHECK(opt == std::nullopt);
 }
@@ -95,23 +95,23 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(constructible_from_primitive_type, type, basic_typ
 
 BOOST_AUTO_TEST_CASE(test_generic_cast_numeric_to_string)
 {
-	Noz::let i = 1;
-	BOOST_CHECK(Noz::test_generic_cast<bool       >(i) == "static_cast" );
-	BOOST_CHECK(Noz::test_generic_cast<char       >(i) == "static_cast" );
-	BOOST_CHECK(Noz::test_generic_cast<short      >(i) == "static_cast" );
-	BOOST_CHECK(Noz::test_generic_cast<float      >(i) == "static_cast" );
-	BOOST_CHECK(Noz::test_generic_cast<double     >(i) == "static_cast" );
-	BOOST_CHECK(Noz::test_generic_cast<std::string>(i) == "lexical_cast");
+    Noz::let i = 1;
+    BOOST_CHECK(Noz::test_generic_cast<bool>(i) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<char>(i) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<short>(i) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<float>(i) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<double>(i) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<std::string>(i) == "lexical_cast");
 }
 
 BOOST_AUTO_TEST_CASE(test_generic_cast_string_to_numeric)
 {
     Noz::let s = "1";
-    BOOST_CHECK(Noz::test_generic_cast<bool       >(s) == "static_cast");
-    BOOST_CHECK(Noz::test_generic_cast<char       >(s) == "static_cast");
-    BOOST_CHECK(Noz::test_generic_cast<short      >(s) == "static_cast");
-    BOOST_CHECK(Noz::test_generic_cast<float      >(s) == "static_cast");
-    BOOST_CHECK(Noz::test_generic_cast<double     >(s) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<bool>(s) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<char>(s) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<short>(s) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<float>(s) == "static_cast");
+    BOOST_CHECK(Noz::test_generic_cast<double>(s) == "static_cast");
     BOOST_CHECK(Noz::test_generic_cast<std::string>(s) == "static_cast");
 }
 
@@ -139,21 +139,21 @@ BOOST_AUTO_TEST_CASE(left_right_substr)
 
 BOOST_AUTO_TEST_CASE(fixed_string_default_construct)
 {
-	Noz::fixed_string<10> s{};
+    Noz::fixed_string<10> s{};
 }
 
 BOOST_AUTO_TEST_CASE(fixed_string_construct_from_std_string)
 {
-	BOOST_CHECK((Noz::fixed_string<4>{std::string{"abc"}} == "abc"));
-	BOOST_CHECK((Noz::fixed_string<4>{std::string{"abc"}, 1, 1} == "b"));
-	BOOST_CHECK((Noz::fixed_string<4>{std::string_view{"abc"}, 1, 1} == "b"));
-	BOOST_CHECK((Noz::fixed_string<4>{std::string_view{"abc"}, 1, 1} == "b"));
+    BOOST_CHECK((Noz::fixed_string<4>{std::string{"abc"}} == "abc"));
+    BOOST_CHECK((Noz::fixed_string<4>{std::string{"abc"}, 1, 1} == "b"));
+    BOOST_CHECK((Noz::fixed_string<4>{std::string_view{"abc"}, 1, 1} == "b"));
+    BOOST_CHECK((Noz::fixed_string<4>{std::string_view{"abc"}, 1, 1} == "b"));
 }
 
 BOOST_AUTO_TEST_CASE(fixed_string_compare)
 {
-	BOOST_CHECK(Noz::fixed_string<4>{"abc"} == "abc");
-	BOOST_CHECK(Noz::fixed_string<4>{"abcdef"} == "abc");
-	BOOST_CHECK(Noz::fixed_string<1>{""} == "");
-	BOOST_CHECK(Noz::fixed_string<1>{""}.empty());
+    BOOST_CHECK(Noz::fixed_string<4>{"abc"} == "abc");
+    BOOST_CHECK(Noz::fixed_string<4>{"abcdef"} == "abc");
+    BOOST_CHECK(Noz::fixed_string<1>{""} == "");
+    BOOST_CHECK(Noz::fixed_string<1>{""}.empty());
 }
